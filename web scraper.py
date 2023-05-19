f14f66eb72b0d38a976514f4e274f4ceb9da7a0d
asongg <129704726+asongg@users.noreply.github.com>
2023-05-19 10:51:28 -0400

    Update web scraper.py

--- web scraper.py
+++ web scraper.py
@@ -12,9 +12,11 @@ import json
 from selenium_stealth import stealth
 from seleniumwire import webdriver
 import time
-LOGIN_USER = 'your_username'
-LOGIN_PASSW = 'your_password'
-usernames = ["zyqtcx99"] # usernames you want to scrape
+import re
+LOGIN_USER = 'zyqtcx99'
+LOGIN_PASSW = 'AustinS123'
+# caption_to_scrape = sys.argv[1]
+usernames = ["j.e.bracelets"]
 output = {}
 def prepare_browser():
     chrome_options = webdriver.ChromeOptions()
@@ -22,8 +24,8 @@ def prepare_browser():
     chrome_options.add_argument(f'--proxy-server={proxy}')
     chrome_options.add_argument("start-maximized")
     chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
-    chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
-    chrome_options.add_experimental_option('useAutomationExtension', False)
+    chrome_options.add_experimental_option('excludeSwitches', ['enable-logging']) 
+    chrome_options.add_experimental_option('useAutomationExtension', False) #disables the use of other extensions while on chrome
     driver = webdriver.Chrome(options=chrome_options, service=Service((ChromeDriverManager().install())))
     driver1 = webdriver.Chrome(options= chrome_options)
     stealth(driver,
@@ -37,19 +39,26 @@ def prepare_browser():
     return driver
 def parse_data(username, user_data):
     captions = []
+    posts = []
     if len(user_data['edge_owner_to_timeline_media']['edges']) > 0:
         for node in user_data['edge_owner_to_timeline_media']['edges']:
             if len(node['node']['edge_media_to_caption']['edges']) > 0:
-                if node['node']['edge_media_to_caption']['edges'][0]['node']['text']:
-                    captions.append(
-                        node['node']['edge_media_to_caption']['edges'][0]['node']['text']
-                    )
+                if (node['node']['edge_media_to_caption']['edges'][0]['node']['text']):
+                    exp = re.compile("brown", re.I)
+                    for result in exp.finditer(node['node']['edge_media_to_caption']['edges'][0]['node']['text']):
+                        
+                        captions.append(
+                            node['node']['edge_media_to_caption']['edges'][0]['node']['text']
+                        )
+                        posts.append(
+                            node['node']['display_url']
+                        )
                 
     output[username] = {
         'name': user_data['full_name'],
         'category': user_data['category_name'],
         'followers': user_data['edge_followed_by']['count'],
-        'posts': captions,
+        'posts': posts,
     }
 def scrape(username):
     
@@ -59,8 +68,6 @@ def scrape(username):
     chrome.get(url1)
     username_field = WebDriverWait(chrome, 10).until(EC.visibility_of_element_located((By.NAME, "username")))
     password_field = WebDriverWait(chrome, 10).until(EC.visibility_of_element_located((By.NAME, "password")))
-    # username_el = browser.find_element(By.XPATH, "//span[contains(@id,'username')]")
-    # password_el = browser.find_element(By.NAME,'password')
     time.sleep(3)
     username_field.send_keys(LOGIN_USER)
     time.sleep(3)
@@ -69,7 +76,8 @@ def scrape(username):
     time.sleep(1)
     submit_el.click()
     time.sleep(5)
-    chrome.find_element(By.TAG_NAME, "body").send_keys(Keys.CONTROL + "t")
+    tab_field = WebDriverWait(chrome, 5).until(EC.visibility_of_element_located((By.TAG_NAME, "body")))
+    tab_field.send_keys(Keys.CONTROL + "t")
     chrome.get(url)
     print (f"Attempting: {chrome.current_url}")
     if "login" in chrome.current_url:
@@ -79,7 +87,6 @@ def scrape(username):
         print ("Success")
         resp_body = chrome.find_element(By.TAG_NAME, "body").text
         print(resp_body)
-        input()
         data_json = json.loads(resp_body)
         user_data = data_json['graphql']['user']
         parse_data(username, user_data)
